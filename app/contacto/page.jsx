bb9403741ad5a8297ed109f429bb3c4c2aed0912
Juan Jose <poloj3614@gmail.com>
2025-10-21 14:24:12 -0500

    Agregando funcionalidad de email

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -19,7 +19,7 @@ import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
 import { Textarea } from "../../components/ui/textarea";
 import { Button } from "../../components/ui/button";
-import { useContactForm } from "../../hooks/useContactForm";
+import { useContactForm } from "../../src/hooks/useContactForm";
 import { Alert, AlertDescription } from "../../components/ui/alert";
 
 export default function ContactPage() {
